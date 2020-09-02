989330920464c1f34e3c389432b914d3ef68c0f0
Lily <pixielf.0@gmail.com>
2020-09-02 19:46:07 -0400

    add more item data

--- mka/_build_data.py
+++ mka/_build_data.py
@@ -1,62 +1,129 @@
+import contextlib
 import io
 import json
+import pathlib
+import pandas as pd
 import re
-import pandas
 
 
-class Recipe:
-    def __init__(self, recipe, location, itemName, ingredients):
-        self.recipe = recipe
-        self.location = location
-        self.item = itemName
-        self.ingredients = ingredients
+def htmlify(item, headers):
+    def _gen():
+        for head, width in headers.items():
+            val = item[head]
 
-    def pretty(self, i):
-        ingr = '\n'.join(f'<li>{i}</li>' for i in self.ingredients)
-        color = 'even' if i % 2 == 0 else 'odd'
-        return f'''
-    <tr>
-        <td class="{color}" style="width: 12%;"><b>{self.item}</b></td>
-        <td class="{color}" style="width: 12%;">{self.recipe}</td>
-        <td class="{color}" style="width: 12%;">{self.location}</td>
-        <td class="{color}" style="width: 64%;"><p><ul>{ingr}</ul></p></td>
-    </tr>
-'''
+            if head == 'Ingredients':
+                # format as list
+                try:
+                    val = '<p><ul>' + '\n\t'.join(f'<li>{i}</li>' for i in val) + '</ul></p>'
+                except TypeError:
+                    val = None
+            elif head in ('User', 'Stats'):
+                # format as list
+                try:
+                    val = '<p><ul>' + '\n\t'.join(f'<li>{i}</li>' for i in re.split(',\s*', val)) + '</ul></p>'
+                except TypeError:
+                    val = None
+            elif head == 'Sell':
+                # try to convert to int
+                with contextlib.suppress(ValueError):
+                    val = int(float(val))
+
+            yield f'<td style="width: {width}%;">{val}</td>'
+    return '<tr>' + '\n\t'.join(_gen()) + '</tr>'
 
 
-def find_recipe(lookup, item):
-    g = (r for r in lookup if r['itemName'] == item)
-    return next(g, None)
+recipes = json.loads(pathlib.Path('raw_data/recipe-data.json').read_text())
+itemdata = pd.read_csv('raw_data/item-data.csv', index_col=0)
+
+
+def find_recipe(item):
+    for r in recipes:
+        if r['item_name'] == item:
+            return r
+    raise ValueError(f'Cannot find recipe for item {item!r}')
 
 
 def build_recipes():
+    categories = {
+        'Usable': {
+            'Name': 12,
+            'Synthesis': 12,
+            'Recipe Location': 12,
+            'Ingredients': 56,
+            'Sell': 8
+        },
+
+        'Material': {
+            'Name': 12,
+            'Synthesis': 12,
+            'Recipe Location': 12,
+            'Ingredients': 56,
+            'Sell': 8
+        },
+
+        'Weapon': {
+            'Name': 12,
+            'Synthesis': 12,
+            'Recipe Location': 12,
+            'Ingredients': 40,
+            'User': 8,
+            'Stats': 8,
+            'Sell': 8
+        },
+
+        'Armor': {
+            'Name': 12,
+            'Synthesis': 12,
+            'Recipe Location': 12,
+            'Ingredients': 40,
+            'User': 8,
+            'Stats': 8,
+            'Sell': 8
+        },
+
+        'Accessory': {
+            'Name': 12,
+            'Synthesis': 12,
+            'Recipe Location': 12,
+            'Ingredients': 40,
+            'User': 8,
+            'Stats': 8,
+            'Sell': 8
+        },
+
+        'Key': {
+            'Name': 12,
+            'Synthesis': 12,
+            'Recipe Location': 12,
+            'Ingredients': 56,
+            'Sell': 8
+        }
+    }
+
     stream = io.StringIO()
-    items = pandas.read_csv('raw_data/item-data.csv', index_col=0)
-    with open('raw_data/recipe-data.json', 'r') as f:
-        recipes = json.load(f)
-
-    for category in ('Usable', 'Material', 'Weapon', 'Armor', 'Accessory', 'Key'):
+    for category, headers in categories.items():
+        # write the HTML header for this category
         stream.write(f'''
-    <button type="button" class="collapsible">{category} Items</button>
-    <div class="content">
-    <table>
-    <tr>
-        <th style="width: 12%;">Item Name</th>
-        <th style="width: 12%;">Recipe Name</th>
-        <th style="width: 12%;">Recipe Location</th>
-        <th style="width: 64%;">Ingredients</th>
-    </tr>
+        <button type="button" class="collapsible">{category} Items</button>
+        <div class="content">
+            <table>
+            <tr>
 ''')
-        filtered = items[items.Category == category]
-        i = 0
-        for item in filtered.Name:
-            if not (k := find_recipe(recipes, item)):
-                continue
+        for head, width in headers.items():
+            stream.write(f'<th style="width: {width}%;">{head}</th>\n')
+        stream.write('</tr>\n')
 
-            stream.write(Recipe(**k).pretty(i))
-            i += 1
-
-        stream.write('</table></div>')
+        # get the synthesizable items which fit into the given category
+        filtered = itemdata[(itemdata.Category == category)].dropna(subset=('Synthesis',))
+        for row in filtered.to_dict(orient='record'):
+            item = {
+                k: row.get(k, None)
+                for k in headers.keys()
+            }
+            r = find_recipe(row['Name'])
+            item['Recipe Location'] = r['recipe_location']
+            item['Ingredients'] = r['ingredients']
+            stream.write(htmlify(item, headers) + '\n')
 
     with open('x.txt', 'w+') as f:
         stream.seek(0)
