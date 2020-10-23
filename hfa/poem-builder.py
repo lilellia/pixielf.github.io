c4d33cffd46cd546c4ec2875b626a6b7106de967
Lily <pixielf.0@gmail.com>
2020-10-23 02:11:27 -0400

    test moving HFA poems to individual files

---
+++ hfa/poem-builder.py
@@ -0,0 +1,134 @@
+import contextlib
+import io
+import itertools
+import pathlib
+import re
+from typing import List
+
+
+HERE = pathlib.Path(__file__).parent
+
+
+def bisect_iter(iterable, key=bool):
+    a, b = itertools.tee(iter(iterable), 2)
+    return itertools.takewhile(key, a), itertools.dropwhile(key, b)
+
+
+@contextlib.contextmanager
+def tag(stream, tagname, **kwargs):
+    stream.write(f'<{tagname}')
+    stream.write(''.join(f' {k}="{v}"' for k, v in kwargs.items()))
+    stream.write('>\n')
+
+    try:
+        yield
+    finally:
+        stream.write(f'\n</{tagname}>')
+
+
+class Poem:
+    def __init__(self, title: str, id: str, stanzas: List[List[str]]):
+        self.title = title
+        self.id = id
+        self.stanzas = stanzas
+
+    @classmethod
+    def from_file(cls, path: pathlib.Path):
+        lines = iter(path.read_text().splitlines())
+
+        # strip out title data
+        _title, lines = bisect_iter(lines, lambda line: line.startswith('TITLE'))
+        title = '<br/>\n\t'.join(
+            parse(re.match(r'TITLE: (.*)', line).group(1))
+            for line in _title
+        )
+
+        # strip out ID data
+        _id, lines = bisect_iter(lines, lambda line: line.startswith('ID'))
+        poemid = [
+            parse(re.match(r'ID: (.*)', line).group(1))
+            for line in _id
+        ]
+        if len(poemid) == 0:
+            raise ValueError(f'No poem ID found in {path}')
+        elif len(poemid) > 1:
+            raise ValueError(f'Multiple poem IDs found in {path}')
+
+        # parse stanzas
+        stanzas = [[]]
+        while not (line := next(lines)).rstrip():
+            # ignore blank lines at the start
+            pass
+
+        lines = list(lines)
+
+        while lines:
+            if (line := line.rstrip()):
+                stanzas[-1].append(parse(line))
+            else:
+                stanzas.append([])
+
+            line = lines.pop(0)
+
+        return cls(title=title, id=poemid[0], stanzas=stanzas)
+
+    def htmlify(self):
+        s = io.StringIO()
+
+        s.write(f'<h1 class="poemtitle" id="{self.id}">\n\t{self.title}\n</h1>\n')
+        with tag(s, tagname='div', **{'class': 'poem'}):
+            for stanza in self.stanzas:
+                with tag(s, tagname='p'):
+                    s.write('<br/>\n'.join(stanza))
+
+        s.seek(0)
+        return s.read()
+
+
+def parse(text):
+    # handle <ja>...</ja> tags
+    text = re.sub(r'<ja>(.*?)</ja>', '<span class="japanese">\g<1></span>', text)
+
+    # convert *...* to <i>...</i>
+    text = re.sub(r'\*(.*?)\*', '<i>\g<1></i>', text)
+
+    # convert _..._ to underline
+    text = re.sub(r'_(.*?)_', '<span class="underline">\g<1></span>', text)
+
+    # handle leading spaces
+    text = re.sub(r'^\s+', '&nbsp;&nbsp;', text)
+
+    return text
+
+
+with open(HERE / 'hfa.html', 'w+') as f:
+    with tag(f, tagname='html'):
+        with tag(f, tagname='head'):
+            with tag(f, tagname='title'):
+                f.write('Humanity of a Former Automaton\n')
+            f.write('''<link href="https://fonts.googleapis.com/css?family=Tangerine" rel="stylesheet" />\n''')
+            f.write('''<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" />\n''')
+            f.write('''<link href="../styles/base-style.css" rel="stylesheet" />\n''')
+            f.write('''<link href="style.css" rel="stylesheet" />\n''')
+            f.write('''<link rel="icon" href="../favicon.png" type="image/x-icon">''')
+            f.write('''<meta name="viewport" content="width=device-width, initial-scale=1">''')
+
+        with tag(f, tagname='body', style='font-family: Montserrat, sans-serif;'):
+            # write page title
+            with tag(f, tagname='div', **{'class': 'main'}):
+                with tag(f, tagname='h1', **{'class': 'title'}):
+                    f.write('Humanity of a Former Automaton\n')
+                with tag(f, tagname='h2', **{'class': 'title'}):
+                    f.write('Lily Ellington\n')
+
+                # write each poem
+                navbar = []
+                for path in sorted((HERE / 'poems').iterdir()):
+                    poem = Poem.from_file(path)
+                    navbar.append(poem)
+                    f.write(poem.htmlify())
+
+            # build navbar
+            with tag(f, tagname='div', **{'class': 'navbar'}):
+                for poem in navbar:
+                    f.write(f"""<a href="#{poem.id}">{poem.title}</a><hr class="navdivider">""")
